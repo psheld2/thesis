801721ec8b1a59fa21d90587b7c09705e16c0e79
Ubuntu <ubuntu@ip-172-31-93-155.ec2.internal>
2026-02-19 02:35:59 +0000

    more accessibility cleanup

--- generate_epub.sh
+++ generate_epub.sh
@@ -5,6 +5,7 @@ unzip thesis.epub
 patch OPS/content.opf ../patches/content.patch
 patch OPS/thesis.xhtml ../patches/thesis.xhtml.patch
 patch OPS/nav.xhtml ../patches/nav.xhtml.patch
+patch OPS/nav.xhtml ../patches/nav.xhtml.patch.2
 zip ../thesis_c.epub mimetype META-INF/ META-INF/* OPS/ OPS/*
 cd ..
 rm -rf tmp
