5887bd907e20e5dccedbf0a88b4a79bde3c104e2
Ubuntu <ubuntu@ip-172-31-93-155.ec2.internal>
2026-02-19 15:28:49 +0000

    updates to generate epub

--- generate_epub.sh
+++ generate_epub.sh
@@ -6,6 +6,6 @@ patch OPS/content.opf ../patches/content.patch
 patch OPS/thesis.xhtml ../patches/thesis.xhtml.patch
 patch OPS/nav.xhtml ../patches/nav.xhtml.patch
 patch OPS/nav.xhtml ../patches/nav.xhtml.patch.2
-zip ../thesis_c.epub mimetype META-INF/ META-INF/* OPS/ OPS/*
+zip ../thesis_c.epub mimetype META-INF/ META-INF/* OPS/ OPS/* OPS/tikz_plots OPS/tikz_plots/*
 cd ..
 rm -rf tmp
