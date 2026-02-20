03c8765505b2691093227ece80f528f293c8e99c
Ubuntu <ubuntu@ip-172-31-93-155.ec2.internal>
2026-02-20 01:05:12 +0000

    TIKZ plot updates for DoR TIER

---
+++ tikz_plots/generate_images.sh
@@ -0,0 +1,8 @@
+pdflatex TIERDecodingOverview_wNotInk_n2.tex
+pdflatex TIERDecodingOverview_wInk_n2.tex
+pdflatex DoRDecodingOverview_n2.tex
+pdflatex DoRDecodingOverview_n4.tex
+pdftoppm -png -r 1200 DoRDecodingOverview_n2.pdf DoRDecodingOverview_n2
+pdftoppm -png -r 1200 DoRDecodingOverview_n4.pdf DoRDecodingOverview_n4
+pdftoppm -png -r 1200 TIERDecodingOverview_wNotInK_n2.pdf TIERDecodingOverview_wNotInK_n2
+pdftoppm -png -r 1200 TIERDecodingOverview_wInK_n2.pdf TIERDecodingOverview_wInK_n2
