adfd8c916698c1cbe17f15ebdb2d7371e893c51f
Ubuntu <ubuntu@ip-172-31-93-155.ec2.internal>
2026-02-22 16:46:13 +0000

    updates

--- tikz_plots/generate_images.sh
+++ tikz_plots/generate_images.sh
@@ -8,3 +8,7 @@ pdftoppm -png -r 1200 TIERDecodingOverview_wNotInK_n2.pdf TIERDecodingOverview_w
 pdftoppm -png -r 1200 TIERDecodingOverview_wInK_n2.pdf TIERDecodingOverview_wInK_n2
 pdflatex --shell-escape lambdaExplore.tex
 pdftoppm -png -r 1200 lambdaExplore.pdf lambdaExplore 
+pdflatex PerformanceResults_DorToTier.tex
+pdflatex PerformanceResults_IncreaseL.tex
+pdftoppm -png -r 1200 PerformanceResults_DorToTier.pdf PerformanceResults_DorToTier 
+pdftoppm -png -r 1200 PerformanceResults_IncreaseL.pdf PerformanceResults_IncreaseL
